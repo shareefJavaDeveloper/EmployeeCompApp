ef566f9205292154b524981a28e6eff84912ac59
shareef <shareef@example.com>
2021-09-18 21:55:37 +0530

    Initial Commit

---
+++ src/main/java/com/comp/EmployeeCompAppApplication.java
@@ -0,0 +1,13 @@
+package com.comp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeCompAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeCompAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/comp/EmployeeCompAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeCompAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

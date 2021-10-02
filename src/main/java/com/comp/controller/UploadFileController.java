6c02e11360553b2f1d637a64db7564755554cc22
shareef <shareef@example.com>
2021-10-02 14:27:15 +0530

    setting up jsp-WAR

--- src/main/java/com/comp/controller/UploadFileController.java
+++ src/main/java/com/comp/controller/UploadFileController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.comp.Service.FileService;
@@ -18,12 +19,14 @@ public class UploadFileController {
 	
 	 @GetMapping("/")
 	    public String index() {
-	        return "multipartfile/uploadform.html";
+	        return "index";
 	    }
 	 
 	 @PostMapping("/")
 	    public String uploadMultipartFile(@RequestParam("uploadfile") MultipartFile file) {
 			fileService.store(file);
-	        return "multipartfile/uploadform.html";
+	        return "index";
 	    }
 }
+
+ 
